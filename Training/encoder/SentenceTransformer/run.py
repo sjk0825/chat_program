2bfa7c7384c809219a513ae26a6b4739b55d3cfa
sjk0825 <jikyeong.son@yonsei.ac.kr>
2025-06-18 14:30:49 +0900

    add config.json

--- Training/encoder/SentenceTransformer/run.py
+++ Training/encoder/SentenceTransformer/run.py
@@ -1,4 +1,5 @@
 
+import json
 from datasets import load_dataset
 from sentence_transformers import (
     SentenceTransformer,
@@ -10,8 +11,12 @@ from sentence_transformers.losses import MultipleNegativesRankingLoss
 from sentence_transformers.training_args import BatchSamplers
 from sentence_transformers.evaluation import TripletEvaluator # cosine, dot, uclidean
 
+config = './config/common.json'
+with open(config,'r',encoding='utf-8') as f:
+    config = json.load(f)
+
 model = SentenceTransformer(
-    "microsoft/mpnet-base",
+    config['SentenceTransformer']['name'],
     model_card_data=SentenceTransformerModelCardData(
         language="en",
         license="apache-2.0",
@@ -19,38 +24,51 @@ model = SentenceTransformer(
     )
 )
 
-dataset = load_dataset("sentence-transformers/all-nli", "triplet")
-train_dataset = dataset["train"].select(range(100_000))
+dataset = load_dataset(config['dataset']['name'], "triplet")
+train_dataset = dataset["train"].select(range(config['dataset']['train_size']))
 eval_dataset = dataset["dev"]
 test_dataset = dataset["test"]
 
-loss = MultipleNegativesRankingLoss(model)
+if config['loss'] == 'MultipleNegativesRankingLoss':
+    loss = MultipleNegativesRankingLoss(model)
+else:
+    raise NotImplementedError 'add loss logic'
 
+training_args = config['SentenceTransformerTrainingArguments']
 args = SentenceTransformerTrainingArguments(
-    output_dir="models/mpnet-base-all-nli-triplet",
-    num_train_epochs=1,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    learning_rate=2e-5,
-    warmup_ratio=0.1,
-    fp16=True, 
-    bf16=False, 
+    output_dir=training_args['output_dir'],
+    num_train_epochs=training_args[''],
+    per_device_train_batch_size=training_args[''],
+    per_device_eval_batch_size=training_args[''],
+    learning_rate=training_args['learning_rate'],
+    warmup_ratio=training_args['warmup_ratio'],
+    fp16=training_args['fp16'], 
+    bf16=training_args['bf16'], 
     batch_sampler=BatchSamplers.NO_DUPLICATES, 
-    eval_strategy="steps",
-    eval_steps=100,
-    save_strategy="steps",
-    save_steps=100,
-    save_total_limit=2,
-    logging_steps=100,
-    run_name="mpnet-base-all-nli-triplet",  
+    eval_strategy=training_args['eval_strategy'],
+    eval_steps=training_args['eval_steps'],
+    save_strategy=training_args['save_strategy'],
+    save_steps=training_args['save_steps'],
+    save_total_limit=training_args['save_total_limit'],
+    logging_steps=training_args['logging_steps'],
+    run_name=training_args['run_name'],  
 )
 
-dev_evaluator = TripletEvaluator(
-    anchors=eval_dataset["anchor"],
-    positives=eval_dataset["positive"],
-    negatives=eval_dataset["negative"],
-    name="all-nli-dev",
-)
+if evaluator == 'TripletEvaluator':
+    dev_evaluator = TripletEvaluator(
+        anchors=eval_dataset["anchor"],
+        positives=eval_dataset["positive"],
+        negatives=eval_dataset["negative"],
+        name="all-nli-dev",
+    )
+    test_evaluator = TripletEvaluator(
+        anchors=test_dataset["anchor"],
+        positives=test_dataset["positive"],
+        negatives=test_dataset["negative"],
+        name="all-nli-test",
+    )
+else:
+    raise NotImplementedError "please implement evaluator logic"
 dev_evaluator(model)
 
 trainer = SentenceTransformerTrainer(
@@ -63,14 +81,8 @@ trainer = SentenceTransformerTrainer(
 )
 trainer.train()
 
-test_evaluator = TripletEvaluator(
-    anchors=test_dataset["anchor"],
-    positives=test_dataset["positive"],
-    negatives=test_dataset["negative"],
-    name="all-nli-test",
-)
 test_evaluator(model)
 
-model.save_pretrained("models/mpnet-base-all-nli-triplet/final")
+model.save_pretrained(config['save'])
 
-model.push_to_hub("mpnet-base-all-nli-triplet")
+model.push_to_hub(config['push_to_hub'])
